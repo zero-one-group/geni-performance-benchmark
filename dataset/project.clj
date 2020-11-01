4b950593da575be9ae9ea92274cdb9e95ab7d631
Chris Nuernberger <chris@techascent.com>
2020-11-01 06:57:49 -0700

    partial commit.

--- dataset/project.clj
+++ dataset/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [techascent/tech.ml.dataset "5.00-alpha-17"
+                 [techascent/tech.ml.dataset "5.00-alpha-18"
                   :exclusions [org.apache.commons/commons-compress]]
                  [org.apache.parquet/parquet-hadoop "1.11.0"
                   :exclusions [commons-codec]]
@@ -25,5 +25,4 @@
                  [org.apache.arrow/arrow-memory-netty "2.0.0"]
                  [org.apache.arrow/arrow-memory-core "2.0.0"]
                  [org.apache.arrow/arrow-vector "2.0.0"
-                  :exclusions [commons-codec]]]
-  :jvm-opts ["-Xmx16g"])
+                  :exclusions [commons-codec]]])
--- dataset/src/dataset/optimised_by_chris.clj
+++ dataset/src/dataset/optimised_by_chris.clj
@@ -1,23 +1,22 @@
-(ns dataset.core
+(ns dataset.optimized-by-chris
   (:require
     [tech.v3.io :as io]
     [tech.v3.datatype.functional :as dfn]
     [tech.v3.datatype :as dtype]
     [tech.v3.datatype.casting :as casting]
-    [tech.v3.datatype.datetime :as dtype-dt]
     [tech.v3.dataset :as ds]
     [tech.v3.dataset.reductions :as ds-reduce]
-    [tech.v3.dataset.column :as ds-col]
+    [tech.v3.dataset.utils :as utils]
     [tech.v3.libs.arrow :as arrow]
     [tech.v3.libs.parquet :as parquet]
-    [tech.v3.parallel.for :as parallel-for]
     [primitive-math :as pmath]
-    [clojure.tools.logging :as log]
-    [tech.v3.dataset.utils :as utils])
-  (:import [java.util Map HashMap Set HashSet Map$Entry List]
-           [java.util.function Function Consumer]
+    [clojure.tools.logging :as log])
+  (:import [java.util Map HashMap Set HashSet Map$Entry List
+            ArrayList]
+           [java.util.concurrent ConcurrentHashMap]
+           [java.util.function Function Consumer BiConsumer]
            [java.util.stream Stream]
-           [tech.v3.datatype PrimitiveList])
+           [tech.v3.datatype PrimitiveList IndexReduction])
   (:gen-class))
 
 
@@ -71,74 +70,71 @@
                    ^long n-styles])
 
 
+(defn- aggregate-group-by
+  ^objects [ds-seq]
+  (let [_ (log/info "group-by")
+        results (ArrayList. 10000)]
+    (->> ds-seq
+         ;;Add the sales column to all datasets
+         (map #(assoc % "sales"
+                      (dfn/* (% "price")
+                             (% "quantity"))))
+         ;;group by using this reducer followed by this consumer fn.
+         (ds-reduce/group-by-column-aggregate
+          "member-id"
+          (ds-reduce/aggregate-reducer
+           {:summations (ds-reduce/double-reducer ["sales" "price"]
+                                                  ds-reduce/sum-consumer)
+            ;;Since date, brand-id, and style ID are all int32 quantities, it is
+            ;;safe to use a bitmap reducer
+            :unique (ds-reduce/long-reducer ["date" "brand-id" "style-id"]
+                                            ds-reduce/bitmap-consumer)})
+          ;;This is recommended but I didn't see a large result by setting it.
+          {:map-initial-capacity 100000
+           :finalize-type
+           (fn [member-id reduce-data]
+             (let [summations (get reduce-data :summations)
+                   ;;Summations get saved to map with :n-elems and :value
+                   sales (get summations "sales")
+                   price (get summations "price")
+                   ;;bitmaps are just bitmaps
+                   bitmaps (get reduce-data :unique)
+                   date (get bitmaps "date")
+                   brand-id (get bitmaps "brand-id")
+                   style-id (get bitmaps "style-id")
+                   n-sales (double (:value sales))
+                   n-price (double (:value price))
+                   n-elems (long (:n-elems sales))
+                   result {:member-id member-id
+                           :total-spend n-sales
+                           :avg-basket-size (pmath// n-sales (double n-elems))
+                           :avg-price (pmath// n-price (double n-elems))
+                           :n-transactions n-elems
+                           :n-visits (dtype/ecount date)      ;;roaring bitmap
+                           :n-brands (dtype/ecount brand-id)  ;;roaring bitmap
+                           :n-styles (dtype/ecount style-id)}]
+               (locking results
+                 (.add results result))))}))
+    (.toArray results)))
+
+
 (defn dataset-seq->group-by-aggregate
   [ds-seq]
-  (let [columns (HashMap.)
-        _ (log/info "group-by")
-        ^Stream agg-stream
-        (as-> ds-seq ds-seq
-          ;;Add the sales column to all datasets
-          (map #(assoc % "sales"
-                       (dfn/* (% "price")
-                              (% "quantity"))) ds-seq)
-          ;;aggregate-group-by returns a java Stream because that is the only
-          ;;way to parallelize items coming out of a java.util.Map.
-          (ds-reduce/group-by-column-aggregate
-           "member-id"
-           (ds-reduce/aggregate-reducer
-            {:summations (ds-reduce/double-reducer ["sales" "price"]
-                                                   ds-reduce/sum-consumer)
-             ;;Since date, brand-id, and style ID are all int32 quantities, it is safe
-             ;;to use a bitmap reducer
-             :unique (ds-reduce/long-reducer ["date" "brand-id" "style-id"]
-                                             ds-reduce/bitmap-consumer)})
-           ;;This results in a parallel java stream over the entry-set.
-           ds-seq))
-        ;;Partial reduction
-        _ (log/info "Aggregate to array of records")
-        ary-data
-        (-> agg-stream
-            (.map
-             (reify Function
-               (apply [this data]
-                 (let [member-id (first data)
-                       reduce-data (second data)
-                       summations (get reduce-data :summations)
-                       ;;Summations get saved to map with :n-elems and :value
-                       sales (get summations "sales")
-                       price (get summations "price")
-                       ;;bitmaps are just bitmaps
-                       bitmaps (get reduce-data :unique)
-                       date (get bitmaps "date")
-                       brand-id (get bitmaps "brand-id")
-                       style-id (get bitmaps "style-id")
-                       n-sales (double (:value sales))
-                       n-price (double (:value price))
-                       n-elems (long (:n-elems sales))]
-                   {:member-id member-id
-                    :total-spend n-sales
-                    :avg-basket-size (pmath// n-sales (double n-elems))
-                    :avg-price (pmath// n-price (double n-elems))
-                    :n-transactions n-elems
-                    :n-visits (dtype/ecount date)      ;;roaring bitmap
-                    :n-brands (dtype/ecount brand-id)  ;;roaring bitmap
-                    :n-styles (dtype/ecount style-id)
-                    }))))
-            ;;Collect to an array of results
-            (.toArray))
+  (let [ary-data (aggregate-group-by ds-seq)
         ;;Transpose result
-        _ (log/info "Typed Transpose results")
+        _ (log/info "in-place transpose results")
         n-results (alength ary-data)
         first-record (first ary-data)
-        colmap (->> first-record
-                    (map (fn [[k v]]
-                           [k (case (casting/simple-operation-space (dtype/datatype v))
-                                :int64 (dtype/make-reader
-                                        :int64 n-results
-                                        (unchecked-long (get (aget ary-data idx) k)))
-                                :float64 (dtype/make-reader
-                                          :float64 n-results
-                                          (unchecked-double (get (aget ary-data idx) k))))]))
+        colmap
+        (->> first-record
+             (map (fn [[k v]]
+                    [k (case (casting/simple-operation-space (dtype/datatype v))
+                         :int64 (dtype/make-reader
+                                 :int64 n-results
+                                 (unchecked-long (get (aget ary-data idx) k)))
+                         :float64 (dtype/make-reader
+                                   :float64 n-results
+                                   (unchecked-double (get (aget ary-data idx) k))))]))
                     (into {}))
         final-ds (ds/->dataset colmap)]
     (log/info "Writing result")
